714e74c388370d280639cfef760130f456c6fc69
tbcodes <soypacos@hotmail.com>
2019-06-28 20:56:50 +0200

    Comments and URL added.

--- source_code.php
+++ source_code.php
@@ -1,3 +1,8 @@
+/* 
+Create a folder or directory if it does not already exist */
+PHP code used in this video: https://www.youtube.com/watch?v=LuQsL7bY2kA 
+*/
+
 <?php
   $my_dir = "assets";
   if(!is_dir($my_dir)) {
