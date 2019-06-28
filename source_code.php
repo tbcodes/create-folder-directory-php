c08eaa6c2ab5fadc671a67ad08c70fd5c7e2b92f
tbcodes <soypacos@hotmail.com>
2019-06-28 20:51:33 +0200

    PHP code added!

---
+++ source_code.php
@@ -0,0 +1,9 @@
+<?php
+  $my_dir = "assets";
+  if(!is_dir($my_dir)) {
+    mkdir($my_dir);
+    echo "Se ha creado el directorio $my_dir";
+  } else {
+    echo "El directorio $my_dir ya existe!";
+  }
+?>
